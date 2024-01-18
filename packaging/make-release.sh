5277b710fab379f4978b3d7eb4aa3c0b28d68020
Chmouel Boudjnah <chmouel@chmouel.com>
2024-01-18 17:11:53 +0100

    fix release process

--- packaging/make-release.sh
+++ packaging/make-release.sh
@@ -1,30 +1,50 @@
 #!/usr/bin/env bash
 set -euf
-current=$(git describe --tags $(git rev-list --tags --max-count=1))
 VERSION=${1-""}
-[[ -z ${VERSION} ]] && { 
-   echo "Current version is ${current}"
-   read -p "Would you like to bump [M]ajor, Mi[n]or or [P]atch: " ANSWER
-   if [[ ${ANSWER,,} == "m" ]];then
-       mode=major
-   elif [[ ${ANSWER,,} == "n" ]];then
-       mode=minor
-   elif [[ ${ANSWER,,} == "p" ]];then
-       mode=patch
-   else
-       print "no or bad reply??"
-       exit
-   fi
-   VERSION=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_${mode}()))" ${current})
-   [[ -z ${VERSION} ]] && {
-       echo "could not bump version automatically"
-       exit
-   }
-   echo "Releasing ${VERSION}"
-}
-set -x
+PKGNAME=$(basename $(git rev-parse --show-toplevel))
 
-git tag -s ${VERSION} -m "Releasing version ${VERSION}"
-git push --tags origin ${VERSION}
+docker ps -q >/dev/null || exit 1
+
+bumpversion() {
+	current=$(git tag -l | sort -Vr | head -1)
+	echo "Current version is ${current}"
+
+	major=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_major()))" ${current})
+	minor=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_minor()))" ${current})
+	patch=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_patch()))" ${current})
+
+	echo "If we bump we get, Major: ${major} Minor: ${minor} Patch: ${patch}"
+	read -p "To which version you would like to bump [M]ajor, Mi[n]or, [P]atch or Manua[l]: " ANSWER
+	if [[ ${ANSWER,,} == "m" ]]; then
+		mode="major"
+	elif [[ ${ANSWER,,} == "n" ]]; then
+		mode="minor"
+	elif [[ ${ANSWER,,} == "p" ]]; then
+		mode="patch"
+	elif [[ ${ANSWER,,} == "l" ]]; then
+		read -p "Enter version: " -e VERSION
+		return
+	else
+		print "no or bad reply??"
+		exit
+	fi
+	VERSION=$(python3 -c "import semver,sys;print(str(semver.VersionInfo.parse(sys.argv[1]).bump_${mode}()))" ${current})
+	[[ -z ${VERSION} ]] && {
+		echo "could not bump version automatically"
+		exit
+	}
+	echo "Releasing ${VERSION}"
+}
+
+[[ $(git rev-parse --abbrev-ref HEAD) != main ]] && {
+	echo "you need to be on the main branch"
+	exit 1
+}
+[[ -z ${VERSION} ]] && bumpversion
+git tag ${VERSION}
+git push origin ${VERSION}
+mkdir -p dist
+tar czf ./dist/${PKGNAME}-${VERSION}.tar.gz LICENSE mounch.py README.md helpers/ rofi/
+gh release create ${VERSION} ./dist/${PKGNAME}-${VERSION}.tar.gz
 
 ./packaging/aur/build.sh ${VERSION}
